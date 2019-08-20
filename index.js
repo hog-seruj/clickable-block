d5b8fd16e1fe5b1f13f14da03c675abf809da6cd
HOG <hogseruj1@gmail.com>
2019-08-20 18:46:19 +0300

    Added clickable block component code.

---
+++ index.js
@@ -0,0 +1,45 @@
+/**
+ * @file
+ * Clickable block script.
+ */
+const defaultOptions = [
+  {
+    wrapper: '.clickable-block',
+    link: '.clickable-block__link',
+    excludedClass: '.contextual',
+  }
+];
+
+export default ({
+  options = defaultOptions,
+  context = document,
+} = {}) => {
+  for (let i = 0; i < options.length; i++) {
+    let excludedClass = options[i].excludedClass = 'undefined' ? '.contextual' : options[i].excludedClass;
+    Array.prototype.slice.call(context.querySelectorAll(options[i].wrapper)).forEach((el) => {
+      // Click.
+      el.addEventListener('click', function (e) {
+        goToUrl(el, e, excludedClass, options[i].link);
+      });
+
+      // Keyboard.
+      el.addEventListener("keyup", function(event) {
+        // Enter.
+        if (event.keyCode === 13) {
+          goToUrl(el, e, excludedClass, options[i].link);
+        }
+      });
+    });
+  }
+
+  // Go to Url function.
+  let goToUrl = (el, e, excludedClass, link) => {
+    if (!e.target.closest(excludedClass)) {
+      e.preventDefault();
+      Object.defineProperty(top.location, 'href', {
+        writable: true,
+        value: el.querySelector(link).getAttribute('href')
+      });
+    }
+  }
+};
